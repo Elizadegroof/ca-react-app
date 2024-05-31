e95ba2e2ac1f96521d8d92c997f31edd6fd48b2c
Elizadegroof <eliza.degroof@student.ap.be>
2024-05-31 15:28:30 +0200

    Netlify werkt niet

--- src/data/image.js
+++ src/data/image.js
@@ -2,7 +2,6 @@ import fetcher from './_fetcher'
 import useSWR from 'swr'
 
 export default function useImage (station) {
-  /*const { data, error, isLoading } = useSWR(`https://graph.mapillary.com/images?access_token=${process.env.MAPILLARY_TOKEN}&fields=id,thumb_1024_url&bbox=${station.longitude-0.0001},${station.latitude-0.0001},${station.longitude+0.0001},${station.latitude+0.0001}&limit=1`, fetcher)*/
   const { data, error, isLoading } = useSWR(`https://graph.mapillary.com/images?access_token=MLY|7526764584097748|93480a11d7ab1a1369275f91a4fa2cee&fields=id,thumb_1024_url&bbox=${station.longitude-0.0001},${station.latitude-0.0001},${station.longitude+0.0001},${station.latitude+0.0001}&limit=1`, fetcher)
   
   /*console.log('data:', data);
